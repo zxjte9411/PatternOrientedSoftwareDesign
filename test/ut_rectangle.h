c154f588c73ad2f845ac4eeaa7f1fbb1d8a57b85
109598028 <a5852241@gmail.com>
2020-09-18 21:58:12 +0800

    測試 Rectangle.Info

--- test/ut_rectangle.h
+++ test/ut_rectangle.h
@@ -2,7 +2,7 @@
 
 #include "../src/rectangle.h"
 
+TEST(Rectangle, getInfo){
     Rectangle rectangle(1.0, 1.0);
-    // ASSERT_TRUE(false);
-    ASSERT_EQ("Rectangle (3.712, 4.000)", rectangle.info());
+    ASSERT_EQ("Rectangle (1.000, 1.000)", rectangle.info());
 }
