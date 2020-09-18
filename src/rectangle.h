18159c66465d17a68f9675cc7a73886ee520cb56
109598028 <a5852241@gmail.com>
2020-09-18 23:08:52 +0800

    測試 Rectangle

--- src/rectangle.h
+++ src/rectangle.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include <string>
 
 #include "shape.h"
@@ -8,24 +9,39 @@
 class Rectangle: public Shape {
 private:
     double _length, _width;
+
 public: 
-    Rectangle(double length, double width):_length(length), _width(width) {
+    Rectangle(double length, double width) {
         // If the rectangle can't be successfully created,
         // handle the exception by throwing string "This is not a rectangle!"
+        if (length <= 0 || width <= 0) {
+            throw std::string("This is not a rectangle!");
+        }
+        else
+        {
+            _length = length;
+            _width = width;
+        }
     }
     
     double area() const {
         // return the area of the Rectangle.
-        return _length * _width;
+        double answer = _length * _width * 1000;
+        answer -= 0.5;
+        return round(answer) / 1000;
     }
     
     double perimeter() const {
         // return the perimeter of the Rectangle.
-        return _length * 2.0 + _width * 2.0;
+        return _length + _length + _width + _width;
     }
     
-    string info() const {
-        return "Rectangle (3.712, 4.000)";
+    std::string info() const {
+        char buffer[50];
+        double newLength = round(_length * 1000 - 0.5) / 1000;
+        double newWidth = round(_width * 1000 - 0.5) / 1000;
+        sprintf(buffer, "Rectangle (%.3f, %.3f)", newLength, newWidth);
+        return buffer;
         // return the info of the Rectangle.
         // ex. Rectangle (3.712, 4.000)
     }
--- test/ut_rectangle.h
+++ test/ut_rectangle.h
@@ -1,10 +1,13 @@
 #include <gtest/gtest.h>
 
 #include "../src/rectangle.h"
+#define ABS_ERROR 0.001
 
 TEST(Rectangle, getInfo){
-    Rectangle rectangle(1.0, 1.0);
+    Rectangle rectangle = Rectangle(1.0, 1.0);
     ASSERT_EQ("Rectangle (1.000, 1.000)", rectangle.info());
+    rectangle = Rectangle(4.123, 5.3215);
+    ASSERT_EQ("Rectangle (4.123, 5.321)", rectangle.info());
 }
 
 TEST(Rectangle, testCreateRectangle){
@@ -17,5 +20,33 @@ TEST(Rectangle, testCreateRectangle){
     {
         ASSERT_EQ("This is not a rectangle!", e);
     }
+
+    try
+    {
+        Rectangle rectangle(-1.54, 1.0);
+        FAIL();
+    }
+    catch(std::string e)
+    {
+        ASSERT_EQ("This is not a rectangle!", e);
+    }
     ASSERT_NO_THROW(Rectangle(1, 1));
+}
+
+TEST(Rectangle, testGetArea) {
+    Rectangle rectangle = Rectangle(4, 5);
+    ASSERT_NEAR(20.0, rectangle.area(), ABS_ERROR);
+    rectangle = Rectangle(4.123, 5.789);
+    ASSERT_NEAR(23.868, rectangle.area(), ABS_ERROR);
+    rectangle = Rectangle(111, 999);
+    ASSERT_EQ(110889.0, rectangle.area());
+}
+
+TEST(Rectangle, testGetPerimeter) {
+    Rectangle rectangle = Rectangle(4, 5);
+    ASSERT_NEAR(18.0, rectangle.perimeter(), ABS_ERROR);
+    rectangle = Rectangle(4.123, 5.789);
+    ASSERT_NEAR(19.824, rectangle.perimeter(), ABS_ERROR);
+    rectangle = Rectangle(111, 999);
+    ASSERT_NEAR(2220.0, rectangle.perimeter(), ABS_ERROR);
 }
