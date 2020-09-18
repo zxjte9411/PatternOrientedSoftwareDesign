e5f7d3f5f3559da8de2960d8ef9f8678eeae6f9c
109598028 <a5852241@gmail.com>
2020-09-18 21:58:49 +0800

    測試 Rectangle constructor

--- test/ut_rectangle.h
+++ test/ut_rectangle.h
@@ -6,3 +6,16 @@ TEST(Rectangle, getInfo){
     Rectangle rectangle(1.0, 1.0);
     ASSERT_EQ("Rectangle (1.000, 1.000)", rectangle.info());
 }
+
+TEST(Rectangle, testCreateRectangle){
+    try
+    {
+        Rectangle rectangle(0, 1.0);
+        FAIL();
+    }
+    catch(std::string e)
+    {
+        ASSERT_EQ("This is not a rectangle!", e);
+    }
+    ASSERT_NO_THROW(Rectangle(1, 1));
+}
