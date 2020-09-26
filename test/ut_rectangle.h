93badd5b32e162b935f7aeb09b02e64827b57f67
109598028 <a5852241@gmail.com>
2020-09-27 03:20:29 +0800

    ut_ellipse.h 加入 SetUp&TearDown

--- test/ut_rectangle.h
+++ test/ut_rectangle.h
@@ -3,14 +3,27 @@
 #include "../src/rectangle.h"
 #define ABS_ERROR 0.001
 
-TEST(Rectangle, getInfo){
-    Rectangle rectangle = Rectangle(1.0, 1.0);
-    ASSERT_EQ("Rectangle (1.000, 1.000)", rectangle.info());
-    rectangle = Rectangle(4.123, 5.3215);
-    ASSERT_EQ("Rectangle (4.123, 5.321)", rectangle.info());
+class RectangleTesting : public ::testing::Test {
+protected:
+    Rectangle * rectangle; 
+
+    void SetUp() override {
+        rectangle = new Rectangle(2, 2);
+    }
+    void TearDown() override {
+        delete rectangle;
+    }
+};
+
+TEST_F(RectangleTesting, getInfo){
+    delete rectangle;
+    rectangle = new Rectangle(1.0, 1.0);
+    ASSERT_EQ("Rectangle (1.000, 1.000)", rectangle->info());
+    rectangle = new Rectangle(4.123, 5.3215);
+    ASSERT_EQ("Rectangle (4.123, 5.321)", rectangle->info());
 }
 
-TEST(Rectangle, testCreateRectangle){
+TEST_F(RectangleTesting, testCreateRectangle){
     try
     {
         Rectangle rectangle(0, 1.0);
@@ -33,20 +46,26 @@ TEST(Rectangle, testCreateRectangle){
     ASSERT_NO_THROW(Rectangle(1, 1));
 }
 
-TEST(Rectangle, testGetArea) {
-    Rectangle rectangle = Rectangle(4, 5);
-    ASSERT_NEAR(20.0, rectangle.area(), ABS_ERROR);
-    rectangle = Rectangle(4.123, 5.789);
-    ASSERT_NEAR(23.868, rectangle.area(), ABS_ERROR);
-    rectangle = Rectangle(111, 999);
-    ASSERT_EQ(110889.0, rectangle.area());
+TEST_F(RectangleTesting, testGetArea) {
+    delete rectangle;
+    rectangle = new Rectangle(4, 5);
+    ASSERT_NEAR(20.0, rectangle->area(), ABS_ERROR);
+    delete rectangle;
+    rectangle = new Rectangle(4.123, 5.789);
+    ASSERT_NEAR(23.868, rectangle->area(), ABS_ERROR);
+    delete rectangle;
+    rectangle = new Rectangle(111, 999);
+    ASSERT_EQ(110889.0, rectangle->area());
 }
 
-TEST(Rectangle, testGetPerimeter) {
-    Rectangle rectangle = Rectangle(4, 5);
-    ASSERT_NEAR(18.0, rectangle.perimeter(), ABS_ERROR);
-    rectangle = Rectangle(4.123, 5.789);
-    ASSERT_NEAR(19.824, rectangle.perimeter(), ABS_ERROR);
-    rectangle = Rectangle(111, 999);
-    ASSERT_NEAR(2220.0, rectangle.perimeter(), ABS_ERROR);
+TEST_F(RectangleTesting, testGetPerimeter) {
+    delete rectangle;
+    rectangle = new Rectangle(4, 5);
+    ASSERT_NEAR(18.0, rectangle->perimeter(), ABS_ERROR);
+    delete rectangle;
+    rectangle = new Rectangle(4.123, 5.789);
+    ASSERT_NEAR(19.824, rectangle->perimeter(), ABS_ERROR);
+    delete rectangle;
+    rectangle = new Rectangle(111, 999);
+    ASSERT_NEAR(2220.0, rectangle->perimeter(), ABS_ERROR);
 }
