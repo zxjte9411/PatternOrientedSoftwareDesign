4962e9255cfddb981b9ef5d49bf5ec780b72cc47
109598028 <a5852241@gmail.com>
2020-09-27 03:58:02 +0800

    以需求修改判斷邏輯

--- src/ellipse.h
+++ src/ellipse.h
@@ -15,9 +15,11 @@ private:
 public: 
     Ellipse(double semiMajorAxes, double semiMinorAxes) {
         if (semiMajorAxes > 0 && semiMinorAxes > 0 ) {
-            _semiMajorAxes = semiMajorAxes;
-            _semiMinorAxes = semiMinorAxes;
-            return;
+            if (semiMajorAxes >= semiMinorAxes) {
+                _semiMajorAxes = semiMajorAxes;
+                 _semiMinorAxes = semiMinorAxes;
+                return;
+            }
         }
         throw std::string("This is not a ellipse!");
         // If the ellipse can't be successfully created,
