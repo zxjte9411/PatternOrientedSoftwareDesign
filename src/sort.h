a2ec7596cd1878303b244b1d8f0591b2d5ea12d1
109598028 <a5852241@gmail.com>
2020-09-27 04:00:13 +0800

    新增 sort.h

---
+++ src/sort.h
@@ -0,0 +1,39 @@
+#include "shape.h"
+
+#ifndef SORT
+#define SORT
+
+class sort {
+public:
+    bool areaAscendingCompare(Shape *a, Shape *b) {
+        return false;
+    };
+
+    bool areaDescendingCompare(Shape *a, Shape *b) {
+        return false;
+    };
+
+    bool perimeterAscendingCompare(Shape *a, Shape *b) {
+        return false;
+    };
+
+    bool perimeterDescendingCompare(Shape *a, Shape *b) {
+        return false;
+    };
+};
+
+class AscendingCompare{
+private:
+    std::string _feature;
+public:
+    AscendingCompare(std::string feature): _feature(feature) {}
+};
+
+class DescendingCompare{
+private:
+    std::string _feature;
+public:
+    DescendingCompare(std::string feature): _feature(feature) {}
+};
+
+#endif // SORT
