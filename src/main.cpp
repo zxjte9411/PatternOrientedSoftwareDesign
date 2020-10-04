eb259c151d4bdd265375fda86106e4bb6a7fed9a
109598028 <a5852241@gmail.com>
2020-10-04 15:25:15 +0800

    finish main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
-
+#include "terminal.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <regex>
 
 int main(int argc, char const *argv[])
 {
-    /* code */
-    return 0;
+    if (argc == 5) {
+        std::ifstream ipf(argv[1]);
+        std::ofstream opf(argv[2]);
+        std::string fileContext = "";
+        std::stringstream ss;
+        
+        while (std::getline(ipf, fileContext))
+        {
+            ss << std::regex_replace(fileContext, std::regex("[\r\n]"), "") << " ";
+        }
+        ss << std::string(argv[4]) << " " << std::string(argv[3]);
+        opf << Terminal(ss.str()).showResult();
+        ipf.close();
+        opf.close();
+        return 0;
+    }
+    return -1;
 }
