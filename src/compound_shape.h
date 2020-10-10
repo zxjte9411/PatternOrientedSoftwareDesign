24b769febc9e8585cb6d2ca87cb627c9e2b5925c
109598028 <a5852241@gmail.com>
2020-10-11 05:31:23 +0800

    新要求

---
+++ src/compound_shape.h
@@ -0,0 +1,119 @@
+#ifndef COMPOUND_SHAPE_H
+#define COMPOUND_SHAPE_H
+#include "../src/shape.h"
+#include <vector>
+#include <sstream>
+
+class CompoundShape : public Shape {
+private:
+    std::vector<Shape*> * _shapes;
+    void createChecker(std::vector<Shape*>* shapes) {
+        if (shapes->size() < 1) {
+            throw std::string("This is not a compound shape!");
+        }
+    }
+public:
+    CompoundShape(std::string id, std::vector<Shape*>* shapes): Shape(id, "transparent") {
+        createChecker(shapes);
+        _shapes = new std::vector<Shape*>();
+        std::vector<Shape*>::iterator sptr = shapes->begin();
+        for (;sptr<shapes->end();sptr++)
+            _shapes->push_back(*sptr);
+        // The default color of compound shape should be "transparent".
+        // When there's no shape contain in the compound shape,
+        // should throw std::string "This is not a compound shape!"
+        
+        // throw std::string("This is not a compound shape!");
+    }
+
+    double area() const {
+        std::vector<Shape*>::iterator sptr = _shapes->begin();
+        double total = 0.0;
+        for (;sptr<_shapes->end();sptr++) {
+            total += (*sptr)->area();
+        }
+        // return sum of all containing shapes area.
+        return total;
+    }
+
+    double perimeter() const { 
+        std::vector<Shape*>::iterator sptr = _shapes->begin();
+        double total = 0.0;
+        for (;sptr<_shapes->end();sptr++) {
+            total += (*sptr)->perimeter();
+        }
+        // return sum of all containing shapes perimeter.
+        return total;
+    }
+
+    std::string info() const {
+        // return list of all containing shapes info with wrapped of "CompoundShape {}".
+        // ex."Compound Shape {Ellipse (4.000, 3.000), Rectangle (3.000, 4.000), Triangle ([0.000, 0.000], [3.000, 0.000], [0.000, 4.000])}"
+        std::vector<Shape*>::iterator sptr = _shapes->begin();
+        std::string result = "Compound Shape {";
+        for (;sptr<_shapes->end();sptr++) {
+            if (std::distance(sptr, _shapes->end()) == 1)
+                result += (*sptr)->info() + "}";
+            else
+                result += (*sptr)->info() + ", ";
+        }
+        return result;
+    }
+    
+    void addShape(Shape* shape) {
+        _shapes->push_back(shape);
+        // add shape into compound shape.
+    }
+    
+    void deleteShapeById(std::string id) {
+        std::vector<Shape*>::iterator sptr = _shapes->begin();
+        for (; sptr<_shapes->end(); sptr++) {
+            try
+            {
+                if ((*sptr)->color() == "transparent"){
+                    (*sptr)->deleteShapeById(id);
+                    return;
+                }
+                else if (id == (*sptr)->id()) {
+                    _shapes->erase(sptr);
+                    // delete *sptr;
+                    return;
+                }
+            }
+            catch(std::string e){} //先接起來等到真的都沒有，回圈結束後就會擲出例外
+        }
+        throw std::string("Expected delete shape but shape not found");
+        // search and delete a shape through id,
+        // search all the containing shapes and the tree structure below,
+        // if no match of id, throw std::string "Expected delete shape but shape not found"
+    }
+    
+    Shape* getShapeById(std::string id) {
+        std::vector<Shape*>::iterator sptr = _shapes->begin();
+        for (; sptr<_shapes->end(); sptr++) {
+            try
+            {
+                if ((*sptr)->color() == "transparent"){
+                    return (*sptr)->getShapeById(id);
+                }
+                else if (id == (*sptr)->id()) {
+                    return *sptr;
+                }
+            }
+            catch(std::string e){} //先接起來等到真的都沒有，回圈結束後就會擲出例外
+        }
+        throw std::string("Expected get shape but shape not found");
+        // search and return a shape through id,
+        // search all the containing shapes and the tree structure below,
+        // if no match of id, throw std::string "Expected get shape but shape not found"
+        // return nullptr;
+    }
+
+    ~CompoundShape() {
+        std::vector<Shape*>::iterator ptr = _shapes->begin();
+        for (;ptr<_shapes->end(); ptr++) 
+            delete *ptr;
+        delete _shapes;
+    }
+};
+#endif 
---
+++ test/ut_compound_shape.h
@@ -0,0 +1,117 @@
+#include "../src/compound_shape.h"
+#include "../src/rectangle.h"
+#include "../src/triangle.h"
+#include "../src/ellipse.h"
+#include "../src/shape.h"
+#include "../src/two_dimensional_coordinate.h"
+#include "../src/sort.h"
+#include <gtest/gtest.h>
+#include <vector>
+
+class CompoundShapeTesting : public ::testing::Test {
+protected :
+
+    void SetUp() override {
+        shapes = new std::vector<Shape*>();
+        shapes->push_back(new Rectangle("0", "red", 3, 4));
+        triangleVector.push_back(new TwoDimensionalCoordinate(0, 0));
+        triangleVector.push_back(new TwoDimensionalCoordinate(3, 0));
+        triangleVector.push_back(new TwoDimensionalCoordinate(0, 4));
+        shapes->push_back(new Triangle("1", "blue", triangleVector));
+        shapes->push_back(new Rectangle("2", "red", 2, 2));
+        compoundShape = new CompoundShape("20201010", shapes);
+    }
+    
+    void TearDown() override {
+        delete compoundShape;
+        delete shapes;
+    }
+
+    CompoundShape * compoundShape;
+    std::vector<Shape*> * shapes;
+    std::vector<TwoDimensionalCoordinate*> triangleVector;
+
+};
+
+TEST_F(CompoundShapeTesting, createCompoundShape) {
+    // test setUp
+    ASSERT_EQ("transparent", compoundShape->color());
+    ASSERT_EQ("20201010", compoundShape->id());
+    //-----------------------------------------------
+    std::vector<Shape*> * _shapes = new std::vector<Shape*>();
+    try
+    {
+        CompoundShape compoundShape("99", _shapes);
+        FAIL();
+    }
+    catch(std::string e)
+    {
+        delete _shapes;
+        ASSERT_EQ("This is not a compound shape!", e);
+    }    
+}
+
+TEST_F(CompoundShapeTesting, getArea) {
+    ASSERT_EQ(22.0, compoundShape->area());
+}
+
+TEST_F(CompoundShapeTesting, getPerimeter) {
+    ASSERT_EQ(34.0, compoundShape->perimeter());
+}
+
+TEST_F(CompoundShapeTesting, getInfo) {
+    std::string const answer = "Compound Shape {Rectangle (3.000, 4.000), Triangle ([0.000, 0.000], [3.000, 0.000], [0.000, 4.000]), Rectangle (2.000, 2.000)}";
+    ASSERT_EQ(answer, compoundShape->info());
+}
+
+TEST_F(CompoundShapeTesting, addShape) {
+    std::vector<Shape*> * _shapes = new std::vector<Shape*>();
+    _shapes->push_back(new Ellipse("3", 5.0, 4.0));
+    CompoundShape * cs = new CompoundShape("4", _shapes);
+    compoundShape->addShape(cs);
+    std::string const answer = "Compound Shape {Rectangle (3.000, 4.000), Triangle ([0.000, 0.000], [3.000, 0.000], [0.000, 4.000]), Rectangle (2.000, 2.000), Compound Shape {Ellipse (5.000, 4.000)}}";
+    ASSERT_EQ(answer, compoundShape->info());
+}
+
+TEST_F(CompoundShapeTesting, getShapeById) {
+    std::vector<Shape*> * _shapes = new std::vector<Shape*>();
+    _shapes->push_back(new Ellipse("3", 5.0, 4.0));
+    CompoundShape * cs = new CompoundShape("4", _shapes);
+    cs->addShape(compoundShape);
+    
+    ASSERT_EQ("red", cs->getShapeById("0")->color());
+    ASSERT_EQ("blue", cs->getShapeById("1")->color());
+    ASSERT_EQ("red", cs->getShapeById("2")->color());
+    ASSERT_EQ("white", cs->getShapeById("3")->color());
+
+    try
+    {
+        cs->getShapeById("7");
+        FAIL();
+    }
+    catch(std::string e)
+    {
+        ASSERT_EQ("Expected get shape but shape not found", e);
+    }
+
+    try
+    {
+        cs->getShapeById("4");
+        FAIL();
+    }
+    catch(std::string e)
+    {
+        ASSERT_EQ("Expected get shape but shape not found", e);
+    }
+    
+}
+
+TEST_F(CompoundShapeTesting, deleteById) {
+    std::vector<Shape*> * _shapes = new std::vector<Shape*>();
+    _shapes->push_back(new Ellipse("3", 5.0, 4.0));
+    CompoundShape * cs = new CompoundShape("4", _shapes);
+    cs->addShape(compoundShape);
+    cs->deleteShapeById("0");
+    std::string const answer = "Compound Shape {Ellipse (5.000, 4.000), Compound Shape {Triangle ([0.000, 0.000], [3.000, 0.000], [0.000, 4.000]), Rectangle (2.000, 2.000)}}";
+    ASSERT_EQ(answer, cs->info());
+}
