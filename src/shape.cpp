a578744bf10e116728e114b0824e5f1984fd4943
109598028 <a5852241@gmail.com>
2020-10-11 05:30:29 +0800

    實作與定義分開

---
+++ src/shape.cpp
@@ -0,0 +1,25 @@
+#include "shape.h"
+#include <string>
+
+Shape::Shape(std::string id): _id(id), _color("white"){} // interface for default color "white".
+Shape::Shape(std::string id, std::string color): _id(id), _color(color){} // interface for color input by user.
+
+std::string Shape::id() const {
+    return _id;
+} // return id of shape.
+
+std::string Shape::color() const {
+    return _color;
+} // return color of shape.
+
+void Shape::addShape(Shape *shape) {
+    throw std::string("Only compound shape can add shape!");
+} // throw std::string "Only compound shape can add shape!"
+
+void Shape::deleteShapeById(std::string id) {
+    throw std::string("Only compound shape can delete shape!");
+} // throw std::string "Only compound shape can delete shape!"
+
+Shape* Shape::getShapeById(std::string id) {
+    throw std::string("Only compound shape can get shape!");
+} // throw std::string "Only compound shape can get shape!"
